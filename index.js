bd94f4207b14a154bf9b54a2458e810c122997b8
Pim Tournaye <56040665+PimTournaye@users.noreply.github.com>
2023-11-12 16:20:56 +0100

    socket server up to date push

--- index.js
+++ index.js
@@ -1,76 +1,67 @@
-// express setup
-let express = require('express');
-let app = express();
-
-// http setup
-const { createServer } = require('node:http');
-let server = createServer(app);
-
-// socket.io setup 
 const { Server } = require("socket.io");
-const io = new Server(server);
 
-// get request for pages
-app.use('/', express.static('public/start'));
-app.use('/public/main', express.static('public/main'));
+const io = new Server({
+  cors: {
+    origin: "*",
+  },
+});
 
-// global variables 
+let players = [];
+
+// global variables
 // let persona;
 
 io.on("connection", (socket) => {
-    console.log('user connected');
-    // let clientID = socket.id;
-    // console.log(clientID);
+  console.log("user connected", socket.id);
+  //     console.log(clientID);
 
-    // socket.on('data', (data) => {
-    //     console.log(data);
-    //     let personaColor = data.color;
-    //     let personaName = data.username;
-    //     let clientStartID = data.id;
+  //   Give the newly connected socket
+  socket.on("data", (user) => {
+    console.log(user)
+    players.push(user)
+    io.emit("location", players)
+  })
 
+  // Messaging
+  socket.on("message", (mssgInfo) => {
+    console.log("Message from " + mssgInfo.username + ": " + mssgInfo.message);
 
-    //     let persona = {
-    //         // "id": clientStartID,
-    //         "username": personaName,
-    //         "color": personaColor
-    //     }
+    io.emit("message", mssgInfo);
+  });
 
-    //     io.emit('data', persona);
+  //  Location
+  socket.on("location", (user) => {
+    // Find the player index instead of using findIndex
+  const player = players.find(p => p.username === user.username);
 
-    //     // console.log(persona);
-    // })
+  if (player) {
+    // Directly update the player object
+    player.x = user.x;
+    player.y = user.y; 
+    player.z = user.z;
+  }
 
+  // Emit the full array instead of just player locations
+  io.emit("location", players);
+  });
 
-
-    // emiting message to all users and myself
-    socket.on('message', (mssgInfo) => {
-        console.log('Message from ' + mssgInfo.username + ': ' + mssgInfo.message);
-
-        io.emit('message', mssgInfo);
-    })
-
-    //   socket.on('data', (data) => {
-    //     console.log(data);
-    //     personaColor = data.color;
-    //     personaName = data.name;
-    //   })
-
-    //   let persona = {
-    //     "username": personaName,
-    //     "color": personaColor
-    //     }
-
-    //     io.emit('data', persona);
-
-    //   socket.on('test', (data) => console.log(data))
-
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    })
-})
+  socket.on("disconnect", (user) => {
+    const index = players.findIndex((p) => p.id === socket.id);
+    // Remove from the players array
+    console.log('Removed player', players[index]);
+    
+  if(index !== -1) {
+    players.splice(index, 1); 
+  }
+    
+//     Update the locations for all players by emitting
+    io.emit('location', players)
+  });
+});
 
 // listening on localhost: 3000
 let port = process.env.PORT || 3000;
-server.listen(port, () => {
-    console.log('Hi gallery!', port);
-})
+
+io.listen(port, () => {
+  console.log("server online");
+});
